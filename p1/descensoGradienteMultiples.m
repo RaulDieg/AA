b952a34d8228f8a400497af7aee6d2a39f30d2d8
Usuario Local <usuariolocal@localhost.localdomain>
2016-10-26 12:13:02 +0200

    R.lineal multiples var

--- p1/descensoGradienteMultiples.m
+++ p1/descensoGradienteMultiples.m
@@ -2,12 +2,8 @@ function T = descensoGradienteMultiples(T, alfa, iteraciones, datos)
 
   numCols = columns(datos);
   numFils = rows(datos);
-  
-  
-  
+
   for j = 1:iteraciones
-    #probamos que vaya disminuyendo la funcion de coste
-    #miCoste = coste(a, b, datos)
     sumatorio = zeros(numCols - 1, 1);
     for col = 1: numCols - 1
       for fila = 1:numFils
